133f671feea128ccb6920b952ffd68adb0191330
Prasetyo Adi <adi_broklak@yahoo.com>
2019-07-20 09:13:00 +0700

    Add migration file tiket model

---
+++ migrations/20190720020059-ticket.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: (queryInterface, DataTypes) => queryInterface.createTable('ticket', {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    event_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: true,
+      defaultValue: null,
+      references: {
+        model: 'event',
+        key: 'id',
+      },
+    },
+    type: {
+      type: DataTypes.STRING,
+      values: ['PREMIUM', 'REGULER', 'FREE'],
+      allowNull: false,
+    },
+    qty: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    created_at: {
+      type: DataTypes.DATE(),
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE(),
+      defaultValue: DataTypes.NOW,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('ticket'),
+};
