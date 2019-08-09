7c3f6bc37264dbba72d745394bf49a29345df6af
Permadi Wibisono <mbapewe@gmail.com>
2019-08-09 21:57:11 +0700

    Removed unnecessary code

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const config = require('config');
-const cfg = require('../common/config');
 const def = {
   ...config.get('apiConfig'),
   env: process.env.NODE_ENV || 'development',
@@ -11,31 +10,6 @@ const def = {
   }
 };
 // setup default env
-// def.env = process.env.NODE_ENV || 'development';
 process.env.NODE_ENV = def.env;
 
-// def.debug = true;
-// def.https = false;
-// def.host = 'localhost';
-// def.apiHost = 'localhost';
-// def.port = 4000;
-
-// // sequelize config
-// def.sequelize = {};
-// def.sequelize.debug = console.log;
-// def.sequelize.username = 'root';
-// def.sequelize.password = 'root';
-// def.sequelize.database = 'loket';
-// def.sequelize.host = '127.0.0.1';
-// def.sequelize.port = 3310;
-// def.sequelize.dialect = 'mysql';
-
-// // paths
-// const rootDir = path.dirname(__dirname);
-// def.logPath = path.join(rootDir, 'logs');
-
-// cfg.resolveLocalConfig(__dirname, (err, file) => {
-//   // eslint-disable-next-line global-require, import/no-dynamic-require
-//   if (!err) cfg.merge(def, require(file));
-// });
 module.exports = def;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,7 @@
 import path from 'path';
 import chalk from 'chalk';
-import commonConfig from '../common/config';
 import config from '../config';
 import app from './app';
-
-const configDir = path.join(path.dirname(__dirname), 'config');
-commonConfig.resolveLocalConfig(configDir, (err, file) => {
-  if (err) console.log(chalk.red(err.message)); // eslint-disable-line no-console
-  if (!err) console.log(chalk.green(`Loading config file: ${file}`)); // eslint-disable-line no-console
-});
 
 // eslint-disable-next-line no-console
 console.log(chalk.green(`Running on ${chalk.underline(config.env.toUpperCase())} environment`));
